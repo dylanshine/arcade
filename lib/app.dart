6c079befb4cedf701a48bbb21a5768e7f66fd983
Dylan Shine <shinelabs@Dylans-MBP-2.localdomain>
2020-10-12 13:48:58 -0600

    Refactor App out of main.dart

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Arcade',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Container(),
+    );
+  }
+}
